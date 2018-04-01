639d4fa67faaf73def8cace2b1b661ff4c388a4c
Timotej Gale <timotej.gale@gmail.com>
2018-04-01 13:00:14 +0200

    [Login] Implement form locking

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,6 +13,8 @@ export class LoginComponent implements OnInit {
 	model: any = {};
     loading = false;
     error = null;
+    unlocked = true;
+    max_login_attempts = 3;
  
     constructor(
         private router: Router,
@@ -21,21 +23,41 @@ export class LoginComponent implements OnInit {
     ngOnInit() {
         // reset login status
         this.authenticationService.logout();
+
+ 		// check if the user was previously blocked
+ 		let login_attempts = localStorage.getItem('login_attempts');
+ 		if (login_attempts == undefined) {
+        	localStorage.setItem('login_attempts', String(1));
+        }
+        else {
+        	if (parseInt(login_attempts) >= this.max_login_attempts) {
+        		this.unlocked = false;
+        	}
+        }
     }
  
     login() {
         this.loading = true;
         this.authenticationService.login(this.model.email, this.model.password)
-        
-            .subscribe(data => {
+            	.subscribe(data => {
                     //this.authenticationService.token = token;
  
                     // store email and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('user_email', this.model.email);
 					localStorage.setItem('auth_token', data.token);
+					localStorage.removeItem('login_attempts');
 					this.router.navigate(['/projects']);
             	},
             	error => {
+            		let login_attempts = localStorage.getItem('login_attempts');
+
+			    	if (parseInt(login_attempts) >= this.max_login_attempts) {
+			    		this.unlocked = false;
+			    		return;
+			    	}
+			    	else {
+			    		localStorage.setItem('login_attempts', String(parseInt(login_attempts) + 1));
+			    	}
             		this.error = 'E-naslov ali geslo je napačno.';
                     this.loading = false;
                 }
